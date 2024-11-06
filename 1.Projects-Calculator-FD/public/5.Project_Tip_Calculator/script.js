d40e2c616283cb48885ccd7d7ffbbe7ea3905441
binetougaye <fabigaye3@gmail.com>
2024-11-06 19:08:59 +0000

    feat: gérer les nombres négatifs

--- 1.Projects-Calculator-FD/public/5.Project_Tip_Calculator/script.js
+++ 1.Projects-Calculator-FD/public/5.Project_Tip_Calculator/script.js
@@ -7,11 +7,18 @@ function calculateTip() {
         alert("Veuillez remplir tous les champs");
         return;
     }
-    // Calculate the Tip and Total Amount
     const tipAmount = billAmount * (tipPercentage / 100)
     const totalAmount = tipAmount + billAmount
 
-    // Display the Results
-    document.getElementById("tip-amount").textContent = '$' + tipAmount.toFixed(2)
-    document.getElementById("total-amount").textContent = '$' + totalAmount.toFixed(2)
+    // Calculate the Tip and Total Amount
+    if (tipAmount < 0 || tipPercentage < 0) {
+        alert("Veuillez saisir des entiers")
+        return
+    } else {
+        // Display the Results
+        document.getElementById("tip-amount").textContent = '$' + tipAmount.toFixed(2)
+        document.getElementById("total-amount").textContent = '$' + totalAmount.toFixed(2)
+    }
+
+
 }
